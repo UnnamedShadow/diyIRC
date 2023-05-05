af1af97432d10befc3156e4d5dec69bb9f8213a2
UnnamedShadow <u60_matdom_waw@technischools.com>
2023-05-05 15:58:34 +0200

    added basic struct for server

--- README.md
+++ README.md
@@ -24,19 +24,32 @@ python3 client.py
 
 ## Protocol
 
+all messages("\<message>") are sent in base64 encoded
+time is sent in unix timestamp format
+
 ### Sending messages
 
 client: `SAY <message>`
 
 ### receiving messages
 
-server: `MSG <ip> <timestamp> <message>`
-
-### sending commands
-
-client: `CMD <command>`
+client: `GET MSG <time>`
+server: `MSG <json>`
 
 ### geting ip to name table
 
 client: `GET IPT`  
 server: `IPT <json>`
+
+### send name to ip table
+
+client: `SET IPT <name>`
+
+### getting ping
+
+client: `GET PING`
+server: `PONG <time>`
+
+### invalid request
+
+server: `ERR <message>`
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import shared
 import socket
 import threading
+import time
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # serve at localhost port 1234
@@ -11,6 +12,29 @@ def handle_conn(s,addr):
     shared.send(s, 'hi there '+str(addr))
     # recieve a message
     print(shared.recieve(s))
+    while True:
+        # recieve a message
+        msg = shared.recieve(s)
+        if msg=='':
+                # if the message is empty, the client has disconnected I think
+                s.close()
+                break
+        elif msg[:3]=='SAY':
+            pass
+        elif msg[:7] == 'GET MSG':
+            pass
+        elif msg=='GET IPT':
+            with open('ipt.txt', 'r') as f:
+                shared.send(s, 'IPT '+f.read())
+        elif msg[:7]=='SET IPT':
+            pass
+        elif msg[:8]=='GET PING':
+            #  return the time in unix timestamp format
+            shared.send(s, 'PONG '+str(time.time()))
+        else:
+                shared.send(s, 'ERR '+msg)
+                print('error:')
+                print(msg)
 
 # accept a connection
 while True:
